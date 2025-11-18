bc6caaa9e8dd43a5861f988edb3e50ae726dae92
PROJECTSLV <129120960+PROJECTSLV@users.noreply.github.com>
2025-11-18 22:54:16 +0300

    Update ui.py
    out_v1_ui_create

---
+++ GeometryDashClone/game/ui.py
@@ -0,0 +1,26 @@
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty, StringProperty
+
+class ScoreDisplay(Label):
+    score = NumericProperty(0)
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.text = f"Score: {self.score}"
+        self.font_size = '20sp'
+        self.color = (1, 1, 1, 1)
+        self.bold = True
+    
+    def on_score(self, instance, value):
+        self.text = f"Score: {self.score}"
+
+class GameOverDisplay(BoxLayout):
+    final_score = NumericProperty(0)
+    high_score = NumericProperty(0)
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.size_hint = (0.6, 0.4)
+        self.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
